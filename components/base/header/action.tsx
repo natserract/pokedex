e9f37abb0b46b1045aa8c66914b53af32809026d
Alfin S <benjaminstwo@gmail.com>
2024-03-24 13:24:59 +0800

    add github link

--- components/base/header/action.tsx
+++ components/base/header/action.tsx
@@ -1,8 +1,12 @@
 import { Suspense } from "react";
+import { Github } from "lucide-react";
+import Link from "next/link";
 
 import { Loading } from "~/components/base/loading";
 import { SearchAction } from "~/components/base/header/action-search";
 import { SortAction } from "~/components/base/header/action-sort";
+
+import { cn } from "~/utils/cn";
 
 export interface Props {
   disableSort?: boolean;
@@ -16,6 +20,19 @@ export function Action({ disableSort }: Props) {
       <Suspense fallback={<Loading />}>
         <SortAction disabled={disableSort} />
       </Suspense>
+
+      <Link
+        href="https://github.com/natserract/pokedex/tree/main"
+        target="_blank"
+        className={cn(
+          "text-foreground/50 hover:text-foreground transition-all duration-200",
+        )}
+      >
+        <Github
+          size={20}
+          className="shrink-0 transition-transform duration-200"
+        />
+      </Link>
     </div>
   );
 }
